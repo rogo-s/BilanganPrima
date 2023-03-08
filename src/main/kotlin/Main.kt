7bf5061b75da9523ca00be010e81edd19d2225ee
Rogo <rogosubandono513@gmail.com>
2023-03-08 19:23:40 +0700

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+import java.util.Scanner
+
+class BilanganPrima(private val angka: Int) {
+
+    fun isPrima(): Boolean {
+        if (angka <= 1) {
+            return false
+        }
+
+        for (i in 2 until angka) {
+            if (angka % i == 0) {
+                return false
+            }
+        }
+
+        return true
+    }
+}
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    print("Masukkan angka: ")
+    val angka = scanner.nextInt()
+
+    val bilanganPrima = BilanganPrima(angka)
+
+    if (bilanganPrima.isPrima()) {
+        println("$angka adalah bilangan prima")
+    } else {
+        println("$angka bukan bilangan prima")
+    }
+}
